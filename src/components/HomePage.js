3489cc423e3491cfa0752aa8991100b48fb92ea5
Veronica Adler <vlynnadler@gmail.com>
2021-11-01 21:15:32 -0400

    responsive design features added

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,7 +8,8 @@ function HomePage() {
             <Header />
             <Container fluid>
                 <Row>
-                    <Col className="p-0">
+                    <Col 
+                        className="p-0 m-0 col-12 col-md-7 col-lg-6">
                     <img 
                         src="assets/redhairedwarrior.jpg"
                         alt="warrior"
@@ -16,7 +17,7 @@ function HomePage() {
                     />
                     </Col>
                     <Col className="text-center">
-                        <h3 className=" mb-4" style={{fontSize: '3rem', marginTop: '13rem'}}>Build your characters with ease.</h3>
+                        <h3 className="mb-4 mt-5" style={{fontSize: '3rem'}}>Build your characters with ease.</h3>
                         <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                         <Button style={{backgroundColor: '#282322', color: 'white'}} size="lg" className="mt-2">Get Started</Button>
                     </Col>
